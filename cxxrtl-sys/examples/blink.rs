5501c937f84f00e6b3c96dcf5e4bcd15e732ea52
Simon Thornington <simon.thornington@gmail.com>
2021-07-04 23:03:37 -0400

    working on vcd support

--- cxxrtl-sys/examples/blink.rs
+++ cxxrtl-sys/examples/blink.rs
@@ -3,6 +3,7 @@ use std::path::{Path, PathBuf};
 use std::ffi::CString;
 use std::process::Command;
 use std::env;
+use c_str_macro::c_str;
 
 pub fn build(sources: &[PathBuf], dest: &Path) {
     let output = Command::new("yosys-config")
@@ -15,17 +16,18 @@ pub fn build(sources: &[PathBuf], dest: &Path) {
     let tmp = env::temp_dir().join("cxxrtl.cpp");
 
     let capi = Path::new(include).join("backends/cxxrtl/cxxrtl_capi.cc");
+    let cvcd = Path::new(include).join("backends/cxxrtl/cxxrtl_vcd_capi.cc");
 
     Command::new("yosys")
         .args(&["-p", &format!("write_cxxrtl {}", tmp.to_string_lossy())])
         .args(sources)
         .status()
         .expect("failed generate cxxrtl code");
-    
+
     Command::new("clang++")
         .args(&["-g", "-O3", "-fPIC", "-shared", "-std=c++14"])
         .arg(format!("-I{}", include))
-        .arg(capi).arg(tmp).arg("-o").arg(dest)
+        .arg(capi).arg(cvcd).arg(tmp).arg("-o").arg(dest)
         .status()
         .expect("failed generate cxxrtl code");
 }
@@ -38,25 +40,36 @@ fn main() {
         let sim = cxxrtl::new(lib).expect("failed to load");
         let top = sim.cxxrtl_design_create();
         let blink = sim.cxxrtl_create(top);
+        let vcd = sim.cxxrtl_vcd_create();
+        sim.cxxrtl_vcd_timescale(vcd, 10, c_str!("ns").as_ptr());
+        sim.cxxrtl_vcd_add_from(vcd, blink);
 
-        let clk_s = CString::new("clk").expect("CString::new failed");
-        let clk = sim.cxxrtl_get(blink, clk_s.as_ptr());
-        let led_s = CString::new("led").expect("CString::new failed");
-        let led = sim.cxxrtl_get(blink, led_s.as_ptr());
+        let clk = sim.cxxrtl_get(blink, c_str!("clk").as_ptr());
+        let led = sim.cxxrtl_get(blink, c_str!("led").as_ptr());
 
         sim.cxxrtl_step(blink);
+        sim.cxxrtl_vcd_sample(vcd, 0);
         let mut prev_led = 0;
         for cycle in 0..1000 {
             *(*clk).next = 0;
             sim.cxxrtl_step(blink);
+            sim.cxxrtl_vcd_sample(vcd, cycle*2 + 0);
             *(*clk).next = 1;
             sim.cxxrtl_step(blink);
+            sim.cxxrtl_vcd_sample(vcd, cycle*2 + 1);
 
             let curr_led = *(*led).curr;
             if prev_led != curr_led {
                 println!("cycle {}, led {}", cycle, curr_led);
                 prev_led = curr_led;
             }
+            {
+                let mut len = 0;
+                let mut buf = ::std::ptr::null();
+                sim.cxxrtl_vcd_read(vcd, &mut buf, &mut len);
+                // TODO convert this buffer and length into a slice and
+                // dump it to a file
+            }
         }
     }
-} 
+}
--- cxxrtl-sys/wrapper.h
+++ cxxrtl-sys/wrapper.h
@@ -1,3 +1,4 @@
 #include <backends/cxxrtl/cxxrtl_capi.h>
+#include <backends/cxxrtl/cxxrtl_vcd_capi.h>
 
 cxxrtl_toplevel cxxrtl_design_create();
